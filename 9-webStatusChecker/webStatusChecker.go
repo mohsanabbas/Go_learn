5a37965cc61aa20485e438fceb6303293c4bc2d1
mohsanabbas <moabbas.ch@gmail.com>
2020-09-24 17:38:03 -0300

    web status checker

---
+++ 9-webStatusChecker/webStatusChecker.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://almundo.com.ar",
+		"https://ww2.cvc.com.br/",
+	}
+
+	for _, link := range links {
+		checkStatus(link)
+	}
+}
+
+func checkStatus(link string) {
+	res, err := http.Get(link) // we are sitting here waiting for response for each link get req we will solve this via Go routine and channels
+	if err != nil {
+		fmt.Println(link, "might fucked up")
+		return
+	}
+	fmt.Println(link, "is good and running with", res.Status)
+	// fmt.Println(io.Copy(os.Stdout, res.Body))
+
+}
