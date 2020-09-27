91511a9019839aa6d28ed04d4d32d271dbbebc15
mohsanabbas <moabbas.ch@gmail.com>
2020-09-26 22:53:13 -0300

    continous non blocking call with go routine and chanels

---
+++ 11-go_routine_stauschecker/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://www.youtube.com",
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.facebook.com",
+		"https://www.netflix.com",
+	}
+
+	c := make(chan string)
+	for _, link := range links {
+		go checkStatus(link, c)
+	}
+
+	// for {
+	// 	go checkStatus(<-c, c)
+	// }
+
+	// watch the chanle c and it emits value back assign it to variable l
+	for l := range c {
+		go checkStatus(l, c)
+	}
+
+}
+
+func checkStatus(link string, c chan string) {
+	res, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might fucked up")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is good and running with", res.Status)
+	c <- link
+
+}
